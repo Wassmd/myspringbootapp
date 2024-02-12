4f74b1a6b6a4caf316d669168e29b518e051ceaf
Wasim <mohammed.wasimuddin@rewe-digital.com>
2024-02-12 19:14:29 +0100

    Introduce custom query for finding courses

--- src/main/kotlin/com/paxier/myspringboot/adapter/persistence/CourseJpaRepository.kt
+++ src/main/kotlin/com/paxier/myspringboot/adapter/persistence/CourseJpaRepository.kt
@@ -2,6 +2,13 @@ package com.paxier.myspringboot.adapter.persistence
 
 import com.paxier.myspringboot.domain.entities.CourseEntity
 import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.data.jpa.repository.Query
+import org.springframework.stereotype.Repository
 
+@Repository
 interface CourseJpaRepository: JpaRepository<CourseEntity, Long> {
+    fun findByNameContainsIgnoreCase(name: String): List<CourseEntity>
+
+    @Query("SELECT c FROM CourseEntity c WHERE c.name like %:name%")
+    fun courseByName(name: String):  List<CourseEntity>
 }
---
+++ src/test/intg/com/paxier/myspringboot/adapter/persistence/CourseRepositoryIntgTest.kt
@@ -0,0 +1,66 @@
+package com.paxier.myspringboot.adapter.persistence
+
+import com.paxier.myspringboot.domain.entities.CourseEntity
+import io.kotest.matchers.ints.shouldBeGreaterThanOrEqual
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.Arguments
+import org.junit.jupiter.params.provider.CsvSource
+import org.junit.jupiter.params.provider.MethodSource
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest
+import java.util.stream.Stream
+
+@DataJpaTest
+class CourseRepositoryIntgTest {
+
+    @Autowired
+    lateinit var courseJpaRepository: CourseJpaRepository
+
+    var courses: List<CourseEntity> = emptyList()
+
+    @BeforeEach
+    fun setup() {
+        courses = listOf(
+            CourseEntity(1, "java", "programming"),
+            CourseEntity(2, "iOS", "programming"),
+            CourseEntity(3, "kotlin", "programming"),
+            CourseEntity(3, "Advanced kotlin", "programming")
+        )
+        courseJpaRepository.saveAll(courses)
+    }
+
+
+    @ParameterizedTest
+    //@ValueSource(strings = ["java", "iOS", "kotlin"])
+    @CsvSource(value = ["java, 1", "iOS, 1", "kotlin, 2"])
+    fun `query course by word containing name`(name: String, expectedSize: Int) {
+        val courses = courseJpaRepository.findByNameContainsIgnoreCase(name)
+        courses.size shouldBeGreaterThanOrEqual expectedSize
+    }
+
+    @ParameterizedTest
+    @CsvSource(value = ["java, 1", "iOS, 1", "kotlin, 2"])
+    fun `query course by word containing name 2`(name: String, expectedSize: Int) {
+        val courses = courseJpaRepository.courseByName(name)
+        courses.size shouldBeGreaterThanOrEqual expectedSize
+    }
+
+    @ParameterizedTest
+    @MethodSource("courseAndSize")
+    fun `query course by word containing name 3`(name: String, expectedSize: Int) {
+        val courses = courseJpaRepository.courseByName(name)
+        courses.size shouldBeGreaterThanOrEqual expectedSize
+    }
+
+    companion object {
+        @JvmStatic
+        fun courseAndSize() : Stream<Arguments> {
+            return Stream.of(
+                Arguments.arguments("java", 1),
+                Arguments.arguments("iOS", 1),
+                Arguments.arguments("Kotlin", 2)
+            )
+        }
+    }
+}
