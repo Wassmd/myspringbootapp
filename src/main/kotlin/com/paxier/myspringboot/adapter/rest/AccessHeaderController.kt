373b4272733e7921587ebda2736809dd872a60c6
Wasim <mohammed.wasimuddin@rewe-digital.com>
2024-04-20 23:05:02 +0200

    Access header example

--- src/main/kotlin/com/paxier/myspringboot/adapter/rest/AccessHeaderController.kt
+++ src/main/kotlin/com/paxier/myspringboot/adapter/rest/AccessHeaderController.kt
@@ -2,6 +2,7 @@ package com.paxier.myspringboot.adapter.rest
 
 import org.slf4j.LoggerFactory
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestHeader
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
@@ -10,7 +11,8 @@ import org.springframework.web.bind.annotation.RestController
 class AccessHeaderController {
 
     @GetMapping(produces = ["application/vnd.pid-v2+json"])
-    fun getPId(): PId {
+    fun getPId(@RequestHeader headers: Map<String, String>): PId {
+        headers.forEach { (key, value) -> Logger.debug("Header: $key = $value")  }
         Logger.debug("application/vnd.pid-v2+json")
         return PId("some id from pid-v2+json")
     }
