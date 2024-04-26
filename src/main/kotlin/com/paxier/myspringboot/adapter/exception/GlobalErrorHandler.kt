55e5454bcbc9926ff660aa33421afcaa96118360
Wasim <mohammed.wasimuddin@rewe-digital.com>
2024-04-26 07:36:15 +0200

    Add problem details

--- src/main/kotlin/com/paxier/myspringboot/adapter/exception/GlobalErrorHandler.kt
+++ src/main/kotlin/com/paxier/myspringboot/adapter/exception/GlobalErrorHandler.kt
@@ -3,31 +3,26 @@ package com.paxier.myspringboot.adapter.exception
 import com.paxier.myspringboot.application.exception.InstructorNotFoundException
 import org.slf4j.LoggerFactory
 import org.springframework.http.HttpStatus
+import org.springframework.http.ProblemDetail
 import org.springframework.http.ResponseEntity
-import org.springframework.http.converter.HttpMessageNotReadableException
-import org.springframework.web.bind.MethodArgumentNotValidException
 import org.springframework.web.bind.annotation.ControllerAdvice
 import org.springframework.web.bind.annotation.ExceptionHandler
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler
 
 @ControllerAdvice
-class GlobalErrorHandler {
+class GlobalErrorHandler: ResponseEntityExceptionHandler() {
 
     @ExceptionHandler
-    fun handle(ex: MethodArgumentNotValidException) {
-        LOGGER.error(ex.message, ex)
-        throw(ex)
-    }
-
-    @ExceptionHandler
-    fun handle(ex: HttpMessageNotReadableException) {
-        LOGGER.error(ex.message, ex)
-        throw(ex)
-    }
-
-    @ExceptionHandler
-    fun handle(ex: InstructorNotFoundException): ResponseEntity<Any> {
-        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(ex.message)
-    }
+    fun handle(ex: InstructorNotFoundException): ResponseEntity<Any> =
+        ResponseEntity.of(
+            ProblemDetail.forStatusAndDetail(
+                HttpStatus.BAD_REQUEST,
+                ex::class.simpleName ?: "-"
+            ).apply {
+                title = ex.message
+                setProperty("validationErrors", "Some info")
+            }
+        ).build()
 
     companion object {
         private val LOGGER = LoggerFactory.getLogger(GlobalErrorHandler::class.java)
